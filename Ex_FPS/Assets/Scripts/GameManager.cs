2eff5753dd989cdf6576c1e385d6482a63cc788c
Haesung Jho <bangya1004@naver.com>
2024-11-17 21:41:08 +0900

    Button Edit

--- Ex_FPS/Assets/Scripts/GameManager.cs
+++ Ex_FPS/Assets/Scripts/GameManager.cs
@@ -6,14 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pausePanel;
+
+    void Update()
+    {
+        Pause();
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
     }
 
     public void Exit()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void Continue()
+    {
+        if (Time.timeScale == 0f) Time.timeScale = 1f;
+        pausePanel.SetActive(false);
+    }
+
+    void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1f)
+            {
+                Time.timeScale = 0f;
+                pausePanel.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                pausePanel.SetActive(false);
+            }
+        }
     }
 }
