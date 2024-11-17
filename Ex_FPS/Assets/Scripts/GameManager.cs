20f902a38da1f95da33561836ff61625d992a847
Haesung Jho <bangya1004@naver.com>
2024-11-17 21:09:08 +0900

    TitleScene add.

--- Ex_FPS/Assets/Scripts/GameManager.cs
+++ Ex_FPS/Assets/Scripts/GameManager.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Main");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
