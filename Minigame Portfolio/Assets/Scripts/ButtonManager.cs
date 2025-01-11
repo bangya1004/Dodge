8776375b277b8c8ca3790960f8899c8e0544554a
Haesung Jho <bangya1004@naver.com>
2025-01-11 18:34:37 +0900

    Lobby -> Play Button Setup

--- Minigame Portfolio/Assets/Scripts/ButtonManager.cs
+++ Minigame Portfolio/Assets/Scripts/ButtonManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject panel_StageSelect;
+    [SerializeField]
+    private Button[] SelectGameButtons;
 
     public void OnPanel_Middle()
     {
@@ -15,4 +19,32 @@ public class ButtonManager : MonoBehaviour
     {
         panel_StageSelect.SetActive(false);
     }
+    public void SelectGame(int nums)
+    {
+        for (int i = 0; i < SelectGameButtons.Length; i++)
+        {
+            SelectGameButtons[i].interactable = true;
+        }
+        SelectGameButtons[nums].interactable = false;
+    }
+    public void GamePlay()
+    {
+        if (SelectGameButtons[0].interactable == false)
+        {
+            SceneManager.LoadScene("Stage_Avoid");
+        }
+        else if (SelectGameButtons[1].interactable == false)
+        {
+            SceneManager.LoadScene("Stage_02");
+        }
+        else if (SelectGameButtons[2].interactable == false)
+        {
+            SceneManager.LoadScene("Stage_03");
+        }
+        else if (SelectGameButtons[3].interactable == false)
+        {
+            SceneManager.LoadScene("Stage_04");
+        }
+    }
+
 }
