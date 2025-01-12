3079150c44e01cc0430edbd3df2d79e7d96a0cd8
Haesung Jho <bangya1004@naver.com>
2025-01-12 18:42:54 +0900

    Timer add

---
+++ Minigame Portfolio/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+    private float time;
+
+    private int minute;
+    private int second;
+
+    private void Awake()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    IEnumerator StartTimer()
+    {
+        while (true)
+        {
+            time += Time.deltaTime;
+            minute = (int)time / 60;
+            second = (int)time % 60;
+            timerText.text = minute.ToString("00") + ":" + second.ToString("00");
+            yield return null;
+        }
+    }
+}
