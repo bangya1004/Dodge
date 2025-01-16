56128101cb813c488263c34db0f431c6ac3aae02
Haesung Jho <bangya1004@naver.com>
2025-01-16 20:28:42 +0900

    Stage_02 Add

---
+++ Minigame Portfolio/Assets/Scripts/GroundLoop.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundLoop : MonoBehaviour
+{
+    private float width = 0f;
+    private float speed = 5f;
+
+    private BoxCollider2D groundCollider;
+
+    private void Awake()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        width = groundCollider.size.x;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= -width)
+        {
+            RePosition();
+        }
+    }
+
+    private void RePosition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+}
--- Minigame Portfolio/Assets/Scripts/PlayerMovement.cs
+++ Minigame Portfolio/Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,6 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 1.0f;
-    //[SerializeField]
-    //private GameObject[] lifeImg;
-    //[SerializeField]
-    //private GameObject panel_GameOver;
     [SerializeField]
     private GameManager gameManager;
 
@@ -22,7 +18,9 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer playerSpriteRenderer;
     private Animator playerAnimator;
 
-    //private int lifeCount = 0;
+    private bool isGrounded = false;
+    private float jumpForce = 250.0f;
+    private int jumpCount = 0;
 
     private void Awake()
     {
@@ -35,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Move();
-        //Die();
+        Jump();
     }
 
     private void Move()
@@ -56,28 +54,54 @@ public class PlayerMovement : MonoBehaviour
             }
             transform.position += new Vector3(x, 0, 0) * moveSpeed * Time.deltaTime;
         }
+    }
+
+    private void Jump()
+    {
         if (SceneManager.GetActiveScene().name == "Stage_02")
         {
             playerAnimator.SetBool("isRun", true);
+
+            if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+            {
+                jumpCount++;
+
+                playerRigid.velocity = Vector2.zero;
+                playerRigid.AddForce(new Vector2(0, jumpForce));
+            }
+            else if (Input.GetMouseButtonUp(0) && playerRigid.velocity.y > 0)
+            {
+                playerRigid.velocity = playerRigid.velocity * 0.5f;
+            }
         }
     }
 
     public void Die()
     {
-        //if (lifeCount >= 3)
-        //{
-            Destroy(gameObject);
-            //lifeCount = 0;
-            //panel_GameOver.SetActive(true);
-            //Time.timeScale = 0;
-            // 게임 오버 처리
-        //}
+        Destroy(gameObject);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Spike")
         {
             gameManager.playerCollider();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 바닥에 닿았음을 감지하는 처리
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 바닥에서 벗어났음을 감지하는 처리
+        isGrounded = false;
     }
 }
